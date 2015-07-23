1e19f341d9ab4e456882be972cdba29212ebf207
pratik <lodhapratik9@gmail.com>
2015-07-23 13:49:23 +0530

    backup file creation before update

--- index.js
+++ index.js
@@ -25,8 +25,26 @@ app.use('/',express.static(__dirname));
 app.get('/api/data',supportCrossOriginScript,function(req,res){
 	console.log("yolo");
 	jsonfile.readFile(file, function(err, obj) {
-	  res.send(obj);
+	  if(err){throw err;}
+	  else{
+	  	createbackup(obj)
+	  	res.send(obj);	
+	  }
+	  
 	});
+	function createbackup(obj){
+		console.log("creating one");
+		var filebackup="eventsdata_backup.json";
+		fs.writeFile(filebackup,obj, function (err) {
+			if(err){
+				console.log("backup failed");
+			}
+			else
+			{
+				console.log("data successfull backuped");
+			}
+		});
+	}
 });
 app.post('/checkLogin',urlEncodedParser,supportCrossOriginScript,function(req,res){
 	console.log(req['body']);
@@ -36,11 +54,7 @@ app.post('/checkLogin',urlEncodedParser,supportCrossOriginScript,function(req,re
 		res.send(false);
 });
 app.post('/updateDetails',urlEncodedParser,supportCrossOriginScript,function(req,res){
-	console.log(req['body']['data']);
 	
-	/*jsonfile.writeFile(file, JSON.stringify(JSON.parse(req.body.data),0,4), function (err) {
-  		console.error(err);
-	});*/
 	fs.writeFile(file, req['body']['data'], function (err) {
 
 	});
